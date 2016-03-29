02bb27a9a684f1da36b1251e2b8651300ce02394
Alex <a.juncevich@gmail.com>
2016-03-29 17:18:20 +0500

    Some clean code.

--- src/main/java/sample/util/ExcelDataReader.java
+++ src/main/java/sample/util/ExcelDataReader.java
@@ -1,7 +1,6 @@
 package sample.util;
 
 
-import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
@@ -9,19 +8,15 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import sample.model.Person;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.time.LocalDate;
-import java.time.ZoneId;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 public class ExcelDataReader {
 
     public static List<Person> readFile(String filePath) {
-        List<Person> personList = new ArrayList<Person>();
-        FileInputStream fileInputStream = null;
+        List<Person> personList = new ArrayList<>();
+        FileInputStream fileInputStream;
         String codeSMO;
         String polisVersion;
         String polisNumber;
@@ -34,8 +29,6 @@ public class ExcelDataReader {
 
             // Using XSSF for xlsx format, for xls use HSSF
             Workbook workbook = new XSSFWorkbook(fileInputStream);
-
-            int numberOfSheets = workbook.getNumberOfSheets();
 
             //looping over each workbook sheet
             for (int i = 1; i < 2; i++) {
@@ -54,47 +47,11 @@ public class ExcelDataReader {
                     Person person = new Person(codeSMO , polisVersion, polisNumber, firstName, secondName, lastName, birthday );
                     personList.add(person);
                 }
-                //iterating over each row
-//                while (rowIterator.hasNext()) {
-//
-//                    Person person = new Person();
-//                    Row row = rowIterator.next();
-//                    Iterator cellIterator = row.cellIterator();
 
-                    //Iterating over each cell (column wise)  in a particular row.
-//                    while (cellIterator.hasNext()) {
-//
-//                        Cell cell = cellIterator.next();
-//                        //The Cell Containing String will is name.
-//                        if (Cell.CELL_TYPE_NUMERIC == cell.getCellType()) {
-//                            person.setCodeSMO(cell.getNumericCellValue());
-//
-//                            //The Cell Containing numeric value will contain marks
-//                        } else if (Cell.CELL_TYPE_NUMERIC == cell.getCellType()) {
-//
-//                            //Cell with index 1 contains marks in Maths
-//                            if (cell.getColumnIndex() == 1) {
-//                                student.setMaths(String.valueOf(cell.getNumericCellValue()));
-//                            }
-//                            //Cell with index 2 contains marks in Science
-//                            else if (cell.getColumnIndex() == 2) {
-//                                student.setScience(String.valueOf(cell.getNumericCellValue()));
-//                            }
-//                            //Cell with index 3 contains marks in English
-//                            else if (cell.getColumnIndex() == 3) {
-//                                student.setEnglish(String.valueOf(cell.getNumericCellValue()));
-//                            }
-//                        }
-//                    }
-                    //end iterating a row, add all the elements of a row in list
-
-//                }
             }
 
             fileInputStream.close();
 
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/main/java/sample/view/TableViewController.java
+++ src/main/java/sample/view/TableViewController.java
@@ -3,16 +3,13 @@ package sample.view;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.stage.FileChooser;
-import javafx.stage.Stage;
 import sample.model.Person;
 import sample.util.ExcelDataReader;
 
 import java.io.File;
-import java.time.LocalDate;
 
 public class TableViewController {
     private final ObservableList<Person> usersData = FXCollections.observableArrayList();
@@ -32,8 +29,7 @@ public class TableViewController {
     private TableColumn<Person, String> lastName;
     @FXML
     private TableColumn<Person, String> birthday;
-    @FXML
-    private Button openFile;
+
 
 
     // инициализируем форму данными
@@ -66,7 +62,7 @@ public class TableViewController {
 
 
     public void handleOpenButtonAction() {
-        openFile.setText("Thanks!!!!");
+
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Open Resource File");
         fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("All files", "*.*"),
