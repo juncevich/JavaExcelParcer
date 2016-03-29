0b3c49150cd628b8cbba192ee20a777b2fc17519
Alex <a.juncevich@gmail.com>
2016-03-29 14:33:38 +0500

    Made ExcelDataReader class.

---
+++ src/main/java/sample/util/ExcelDataReader.java
@@ -0,0 +1,7 @@
+package sample.util;
+
+/**
+ * Created by Juncevich on 29.03.2016.
+ */
+public class ExcelDataReader {
+}
--- src/main/java/sample/view/TableViewController.java
+++ src/main/java/sample/view/TableViewController.java
@@ -2,17 +2,14 @@ package sample.view;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.input.MouseEvent;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import sample.model.Person;
 
-import java.awt.event.ActionEvent;
 import java.io.File;
 import java.time.LocalDate;
 
@@ -36,7 +33,6 @@ public class TableViewController {
     private TableColumn<Person, LocalDate> birthday;
     @FXML
     private Button openFile;
-
 
 
     // инициализируем форму данными
@@ -80,12 +76,14 @@ public class TableViewController {
     }
 
 
-
     public void handleOpenButtonAction() {
         openFile.setText("Thanks!!!!");
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Open Resource File");
-        fileChooser.showOpenDialog(new Stage());
+        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("All files", "*.*"),
+                new FileChooser.ExtensionFilter("XLS", "*.xls"));
+
+        File file = fileChooser.showOpenDialog(null);
     }
 
 
